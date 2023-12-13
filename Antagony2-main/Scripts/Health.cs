ede91c459179cacb810230fae2f287e09e4a3053
Slash42 <37580594+Slash42@users.noreply.github.com>
2023-12-13 17:05:48 +0100

    bla

---
+++ Antagony2-main/Scripts/Health.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class Health : Node2D
+{
+    [Export] public float max_health = 100f;
+    float health;
+
+    public override void _Ready() {
+       health = max_health;
+    }
+
+    public void Dmg(float dmg) {
+        health -= dmg;
+
+        if (health <= 0) {
+           GetParent().QueueFree(); 
+        }
+    }
+}
