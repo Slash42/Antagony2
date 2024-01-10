4da180026c9e0cf3429edbe33b0d8b8201f6f0cd
Slash42 <jj.hissnauer@gmail.com>
2024-01-10 02:05:24 +0100

    redid the whole animated sprite thing using AnimationTree

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,43 +7,38 @@ public partial class Player : CharacterBody2D
 	[Export] public float speed = 300f;
 	public float maxHealth = 100;
 	
-	AnimatedSprite2D _idle;
+	AnimationTree animationTree;
 	Vector2 direction = new Vector2(0,0);
 
     public override void _Ready()
     {
-        _idle = GetNode<AnimatedSprite2D>("AntIdle");
+        animationTree = GetNode<AnimationTree>("AnimationTree");
     }
 
     public override void _PhysicsProcess(double delta) {
 		
 		Vector2 mousePos = GetGlobalMousePosition();
-		Vector2 wayToLook = (mousePos - GlobalPosition).Normalized();
-		
+		Vector2 wayToLook = (mousePos - GlobalPosition).Normalized();		
 
 		if (wayToLook.X > 0 && wayToLook.X > Math.Abs(wayToLook.Y)) {
-			direction.X = -100;
-			direction.Y = 0;	 
-		} else if (wayToLook.X < 0 && Math.Abs(wayToLook.X) > Math.Abs(wayToLook.Y)) {
-			direction.X = 100;
+			direction.X = 1;
 			direction.Y = 0;
+		} else if (wayToLook.X < 0 && Math.Abs(wayToLook.X) > Math.Abs(wayToLook.Y)) {
+			direction.X = -1;
+			direction.Y = 0;		
 		} else if (wayToLook.Y > 0 && wayToLook.Y > Math.Abs(wayToLook.X)) {
 			direction.X = 0;
-			direction.Y = -100;
+			direction.Y = -1;
 		} else if (wayToLook.Y < 0 && Math.Abs(wayToLook.Y) > Math.Abs(wayToLook.Y)) {
 			direction.X = 0;
-			direction.Y = 100;
+			direction.Y = 1;
 		}
 
-		LookAt(GlobalPosition - direction);
+		animationTree.Set("parameters/Idle/blend_position", direction);
 
 		Vector2 move_input = Input.GetVector("left", "right", "up", "down");
 
 		Velocity = move_input * speed;
-
-		if (Velocity == Vector2.Zero) {	
-			_idle.Play("AntIdle");
-		}
 
 		MoveAndSlide();
 
