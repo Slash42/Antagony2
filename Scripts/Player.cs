c0f2a5ced2190cb38b4ad598f93c18a301f2f6a2
Slash42 <jj.hissnauer@gmail.com>
2024-01-13 17:24:11 +0100

    Animations, Movement
    Updated character animations again
    Added Acceleration to movement
    Fixed shooting

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,12 +5,15 @@ public partial class Player : CharacterBody2D
 {
 
 	[Export] public float speed = 300f;
-	public float maxHealth = 100;
+	[Export] public float accel = 2f;
+	public float maxHealth = 100f;
 	
 	AnimationTree animationTree;
 	Node2D acidShooter;
 	CollisionShape2D hitbox;
+
 	Vector2 direction = new Vector2(0,0);
+	Vector2 hitboxpos = new Vector2(0,0);
 
     public override void _Ready()
     {
@@ -36,29 +39,39 @@ public partial class Player : CharacterBody2D
 		
 		if (Math.Abs(wayToLook.X) > Math.Abs(wayToLook.Y)) {
 			hitbox.RotationDegrees = 90;
+			hitboxpos.Y = 32;
+			hitboxpos.X = -30;
 			if (wayToLook.X >= 0) {
 				acidShooter.RotationDegrees = 0;
-				direction = Vector2.Right;
+				direction.X = 38;
+				direction.Y = 40;
 			} else {
 				acidShooter.RotationDegrees = 180;
-				direction = Vector2.Left;
+				direction.X = -98;
+				direction.Y = 52;
 			}
 		} else {
 			hitbox.RotationDegrees = 0;
+			hitboxpos.Y = 0;
+			hitboxpos.X = -30;
 			if (wayToLook.Y > 0) {
 				acidShooter.RotationDegrees = 90;
-				direction = Vector2.Down;
+				direction.X = -25;
+				direction.Y = 88;
 			} else {
 				acidShooter.RotationDegrees = 270;
-				direction = Vector2.Up;
+				direction.X = -36;
+				direction.Y = -64;
 			}
 		}
 		
 		acidShooter.Position = direction;
+		hitbox.Position = hitboxpos;
 		
 		Vector2 move_input = Input.GetVector("left", "right", "up", "down");
+		move_input = move_input.Normalized();
 
-		Velocity = move_input * speed;
+		Velocity = Velocity.Lerp(move_input * speed, (float)delta * accel);
 
 		MoveAndSlide();
 
